ee36a1216cc793df6ce14cfa9273c2fb5536b701
The 6th Champion <67034744+The-6th-Champion@users.noreply.github.com>
2021-05-20 13:17:06 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+import os
+import asyncio
+
+from discord.ext import commands
+
+client = discord.Bot(command_prefix="/")
+
+@client.event
+async def on_ready():
+  print("starting")
+  asyncio.sleep(5)
+  send_to =  client.get_channel(781215248790978601)
+  await send_to.send("Im alive i think")
+
+@client.command()
+async def hi(ctx):
+  await ctx.send("\'sup")
+  
+await client.run(os.environ.get("TOKEN"))
