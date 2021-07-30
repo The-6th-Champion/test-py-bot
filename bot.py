e4f33ebbc4d50d96f697926233efaf1695279519
The 6th Champion <67034744+The-6th-Champion@users.noreply.github.com>
2021-07-30 12:44:57 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import asyncio
 
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="!", case_insensitive = True)
 
 @client.event
 async def on_ready():
