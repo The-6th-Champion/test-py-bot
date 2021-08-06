b0bdeebff247dfca36a6bf62cfb97fea99124cb8
The 6th Champion <67034744+The-6th-Champion@users.noreply.github.com>
2021-08-06 13:50:12 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ client = commands.Bot(command_prefix="!", case_insensitive = True)
 @client.event
 async def on_ready():
   print("starting")
-  asyncio.sleep(5)
+  asyncio.sleep(50)
   send_to =  client.get_channel(781215248790978601)
   await send_to.send("Im alive i think")
 
