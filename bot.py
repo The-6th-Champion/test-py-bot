8d42c3a90975231b636353021a253e8a726d05a4
The-6th-Champion <sid1s@T6C-PC-00-R1.localdomain>
2021-08-28 18:26:47 -0700

    bruh

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+import json, flask
+import os
+import asyncio
+
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="!", case_insensitive = True)
+
+@client.event
+async def on_ready():
+  print("starting")
+  asyncio.sleep(50)
+  send_to =  client.get_channel(781215248790978601)
+  await send_to.send("Im alive i think")
+
+@client.command()
+async def hif(ctx):
+  await ctx.send("\'sup")
+  
+client.run(os.environ.get("TOKEN"))
